cf706c05e17b52a924904d6286792b57f64f2542
Arthur Dejardin <arthur.dejardin@gmail.com>
2015-03-08 13:18:26 +0000

    changed order of befor filter in PinsController

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,6 +1,8 @@
 class PinsController < ApplicationController
-  before_action :set_pin, only: [:show, :edit, :update, :destroy]
+ 
   before_filter :authenticate_user!, except: [:index, :show]
+  before_action :set_pin, only: [:show, :edit, :update, :destroy]
+  
   # GET /pins
   # GET /pins.json
   def index
