4f01841a723bcb40bca16e180dcc51d0ba4dc1f9
Arthur Dejardin <arthur.dejardin@gmail.com>
2015-03-08 23:21:57 +0000

    changed the production.rb to try working with s3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,9 +84,9 @@ Rails.application.configure do
 config.paperclip_defaults = {
   :storage => :s3,
   :s3_credentials => {
-    :bucket => "adonemonthrail"
-    :access_key_id => "AKIAINGR4YLDI5AQBFMQ"
-    :secret_access_key => "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"
+    :bucket => "ENV['S3_BUCKET_NAME'],"
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
 }
   
